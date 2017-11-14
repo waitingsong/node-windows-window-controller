c8e368883e8d0bba8a61b34b0175d3bf76451fd6
waiting <1661926154@qq.com>
2017-11-14 15:34:53 +0800

    test: split functions to helper.ts

--- test/20_main.test.ts
+++ test/20_main.test.ts
@@ -9,6 +9,7 @@ import * as sleep from 'mz-modules/sleep';
 import * as nwwc from '../src/index';
 import {conf as GCF, types as GT, windef as WD, conf} from 'win32-api';
 import * as Config from '../src/lib/types';
+import * as H from './helper';
 
 const filename = basename(__filename);
 const Win = nwwc.Win;
@@ -26,6 +27,7 @@ describe(filename, () => {
         let opts: Config.Opts;
 
         child = spawn('calc.exe');
+        H.find_n_check_calc_win();
 
         beforeEach(async () => {
             opts = <Config.Opts> {...Config.filterWinRulesDefaults};
@@ -33,7 +35,7 @@ describe(filename, () => {
             child && child.kill();
             child = spawn('calc.exe');
             await sleep(waitTime);
-            hWnd = find_n_check_calc_win();
+            hWnd = H.find_n_check_calc_win();
             assert(!!user32.IsWindowVisible(hWnd), 'beforeEach: window should visible');
             await sleep(waitTime);
         });
@@ -48,7 +50,7 @@ describe(filename, () => {
                 opts.matchValue = child.pid;
 
                 await nwwc.hide(opts).then(async (execRet) => {
-                    assert_execret(execRet);
+                    H.assert_execret(execRet);
                     const visible = !!user32.IsWindowVisible(hWnd);
                     assert(!visible, ': window should invisible, processed hWnds are "' + execRet.hwnds.join(',') + '"');
                 })
@@ -68,7 +70,7 @@ describe(filename, () => {
 
             try {
                 await nwwc.hide(opts).then(async (execRet) => {
-                    assert_execret(execRet);
+                    H.assert_execret(execRet);
                     const visible = !!user32.IsWindowVisible(hWnd);
                     assert(!visible, ': window should invisible, processed hWnds are "' + execRet.hwnds.join(',') + '"');
                 })
@@ -87,7 +89,7 @@ describe(filename, () => {
 
             try {
                 await nwwc.hide(opts).then(async (execRet) => {
-                    assert_execret(execRet);
+                    H.assert_execret(execRet);
                     const visible = !!user32.IsWindowVisible(hWnd);
                     assert(visible, ': window should visible, processed hWnds are "' + execRet.hwnds.join(',') + '"');
                 })
@@ -107,7 +109,7 @@ describe(filename, () => {
 
             try {
                 await nwwc.hide(opts).then(async (execRet) => {
-                    assert_execret(execRet);
+                    H.assert_execret(execRet);
                     const visible = !!user32.IsWindowVisible(hWnd);
                     assert(!visible, ': window should invisible, processed hWnds are "' + execRet.hwnds.join(',') + '"');
                 })
@@ -126,7 +128,7 @@ describe(filename, () => {
 
             try {
                 await nwwc.hide(opts).then(async (execRet) => {
-                    assert_execret(execRet);
+                    H.assert_execret(execRet);
                     const visible = !!user32.IsWindowVisible(hWnd);
                     assert(visible, ': window should visible, processed hWnds are "' + execRet.hwnds.join(',') + '"');
                 })
@@ -146,7 +148,7 @@ describe(filename, () => {
 
             try {
                 await nwwc.hide(opts).then(async (execRet) => {
-                    assert_execret(execRet);
+                    H.assert_execret(execRet);
                     const visible = !!user32.IsWindowVisible(hWnd);
                     assert( ! visible, 'window should invisible, processed hWnds are "' + execRet.hwnds.join(',') + '"');
                 })
@@ -165,7 +167,7 @@ describe(filename, () => {
 
             try {
                 await nwwc.hide(opts).then(async (execRet) => {
-                    assert_execret(execRet);
+                    H.assert_execret(execRet);
                     const visible = !!user32.IsWindowVisible(hWnd);
                     assert(visible, 'window should visible, processed hWnds are "' + execRet.hwnds.join(',') + '"');
                 })
@@ -185,7 +187,7 @@ describe(filename, () => {
 
             try {
                 await nwwc.hide(opts).then(async (execRet) => {
-                    assert_execret(execRet);
+                    H.assert_execret(execRet);
                     const visible = !!user32.IsWindowVisible(hWnd);
                     assert(visible, ': window should visible, processed hWnds are "' + execRet.hwnds.join(',') + '"');
                 })
@@ -204,7 +206,7 @@ describe(filename, () => {
 
             try {
                 await nwwc.hide(opts).then(async (execRet) => {
-                    assert_execret(execRet);
+                    H.assert_execret(execRet);
                     const visible = !!user32.IsWindowVisible(hWnd);
                     assert(! visible, ': window should invisible, processed hWnds are "' + execRet.hwnds.join(',') + '"');
                 })
@@ -222,7 +224,7 @@ describe(filename, () => {
             opts.matchValue = title;
 
             await nwwc.hide(opts).then((execRet) => {
-                assert_execret(execRet);
+                H.assert_execret(execRet);
                 const visible = !!user32.IsWindowVisible(hWnd);
                 assert(!visible, ': window should invisible, processed hWnds are "' + execRet.hwnds.join(',') + '"');
             })
@@ -237,7 +239,7 @@ describe(filename, () => {
             opts.matchValue = title;
 
             await nwwc.hide(opts).then((execRet) => {
-                assert_execret(execRet);
+                H.assert_execret(execRet);
                 const visible = !!user32.IsWindowVisible(hWnd);
                 assert( ! visible, ': window should invisible, processed hWnds are "' + execRet.hwnds.join(',') + '"');
             })
@@ -251,7 +253,7 @@ describe(filename, () => {
             opts.matchValue = title;
 
             await nwwc.hide(opts).then((execRet) => {
-                assert_execret(execRet);
+                H.assert_execret(execRet);
                 const visible = !!user32.IsWindowVisible(hWnd);
                 assert(visible, ': window should visible, processed hWnds are "' + execRet.hwnds.join(',') + '"');
             })
@@ -300,7 +302,7 @@ describe(filename, () => {
             child && child.kill();
             child = spawn('calc.exe');
             await sleep(waitTime);
-            hWnd = find_n_check_calc_win();
+            hWnd = H.find_n_check_calc_win();
             assert(!!user32.IsWindowVisible(hWnd), 'beforeEach: window should visible');
             await nwwc.hide({...opts, matchType: 'title', matchValue: title});
             await sleep(waitTime);
@@ -318,7 +320,7 @@ describe(filename, () => {
 
             try {
                 await nwwc.show(opts).then(async (execRet) => {
-                    assert_execret(execRet);
+                    H.assert_execret(execRet);
                     if (!execRet.hwnds.length) {
                         assert(false, 'processed hWnds should not empty');
                     }
@@ -341,7 +343,7 @@ describe(filename, () => {
             opts.nCmdShow = 9;
             try {
                 await nwwc.show(opts).then(async (execRet) => {
-                    assert_execret(execRet);
+                    H.assert_execret(execRet);
                     if (!execRet.hwnds.length) {
                         assert(false, 'processed hWnds should not empty');
                     }
@@ -364,7 +366,7 @@ describe(filename, () => {
 
             try {
                 await nwwc.show(opts).then(async (execRet) => {
-                    assert_execret(execRet);
+                    H.assert_execret(execRet);
                     await sleep(waitTime);
                     assert(!user32.IsWindowVisible(hWnd), 'window should invisible');
                 })
@@ -384,7 +386,7 @@ describe(filename, () => {
 
             try {
                 await nwwc.show(opts).then(async (execRet) => {
-                    assert_execret(execRet);
+                    H.assert_execret(execRet);
                     await sleep(waitTime);
                     assert(user32.IsWindowVisible(hWnd), 'window should visible');
                 })
@@ -405,7 +407,7 @@ describe(filename, () => {
 
             try {
                 await nwwc.show(opts).then(async (execRet) => {
-                    assert_execret(execRet);
+                    H.assert_execret(execRet);
                     if (!execRet.hwnds.length) {
                         assert(false, 'processed hWnds should not empty');
                     }
@@ -428,7 +430,7 @@ describe(filename, () => {
 
             try {
                 await nwwc.show(opts).then(async (execRet) => {
-                    assert_execret(execRet);
+                    H.assert_execret(execRet);
                     // some hWnd has not WS_SYSMENU, so comment out
                     // if (execRet.hwnds.length) {
                     //     assert(false, 'processed hWnds should empty');
@@ -453,7 +455,7 @@ describe(filename, () => {
 
             try {
                 await nwwc.show(opts).then(async (execRet) => {
-                    assert_execret(execRet);
+                    H.assert_execret(execRet);
                     await sleep(waitTime);
                     assert( ! user32.IsWindowVisible(hWnd), 'window should invisible');
                 })
@@ -473,7 +475,7 @@ describe(filename, () => {
 
             try {
                 await nwwc.show(opts).then(async (execRet) => {
-                    assert_execret(execRet);
+                    H.assert_execret(execRet);
                     await sleep(waitTime);
                     assert(user32.IsWindowVisible(hWnd), 'window should visible');
                 })
@@ -521,7 +523,7 @@ describe(filename, () => {
             opts.nCmdShow = 9;
 
             await nwwc.show(opts).then(async (execRet) => {
-                assert_execret(execRet);
+                H.assert_execret(execRet);
                 if (!execRet.hwnds.length) {
                     assert(false, 'processed hWnds are empty');
                 }
@@ -542,7 +544,7 @@ describe(filename, () => {
             child && child.kill();
             child = spawn('calc.exe');
             await sleep(waitTime);
-            hWnd = find_n_check_calc_win();
+            hWnd = H.find_n_check_calc_win();
             assert(!!user32.IsWindowVisible(hWnd), 'beforeEach: window should visible');
             await sleep(waitTime);
         });
@@ -576,59 +578,3 @@ describe(filename, () => {
     });
 
 });
-
-
-function find_n_check_calc_win(): GT.HWND {
-    const lpszClass = Buffer.from('CalcFrame\0', 'ucs2');
-    const hWnd = user32.FindWindowExW(null, null, lpszClass, null);
-
-    if (hWnd && !ref.isNull(hWnd) && ref.address(hWnd)) {
-        assert(true);
-        change_title(hWnd, title);
-    }
-    else {
-        assert(false, 'found no calc window, GetLastError: ' + knl32.GetLastError());
-    }
-    return hWnd;
-}
-
-
-function change_title(hWnd: GT.HWND, title: string): void {
-    if (hWnd && !ref.isNull(hWnd) && ref.address(hWnd)) {
-        const res = user32.SetWindowTextW(hWnd, Buffer.from(title + '\0', 'ucs2'));
-
-        if (!res) {
-            // See: [System Error Codes] below
-            const errcode = knl32.GetLastError();
-            const len = 255;
-            const buf = Buffer.alloc(len);
-            const p = 0x00001000 | 0x00000200;  // FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS
-            const langid = 0x0409;              // 0x0409: US, 0x0000: Neutral locale language
-            const msglen = knl32.FormatMessageW(p, null, errcode, langid, buf, len, null);
-
-            if (msglen) {
-                const errmsg = ref.reinterpretUntilZeros(buf, 2).toString('ucs2');
-                assert(false, `window found but change the title failed. errcode: ${errcode}, errmsg: "${errmsg}"`);
-            }
-        }
-        else {
-            const len = title.length;
-            const buf = Buffer.alloc(len * 3);
-            let str: string;
-
-            user32.GetWindowTextW(hWnd, buf, len + 2);
-            str = buf.toString('ucs2').slice(0, len);
-            assert(str === title.trim(), `title should be changed to ${title} in length:${title.length}, bug got ${str} in length:${str.length}`);
-        }
-    }
-    else {
-        assert(false, 'found no calc window, GetLastError: ' + knl32.GetLastError());
-    }
-
-}
-
-function assert_execret(execRet: Config.ExecRet): void {
-    if (execRet.err) {
-        assert(false, execRet.msg);
-    }
-}
---
+++ test/helper.ts
@@ -0,0 +1,74 @@
+/// <reference types="node" />
+/// <reference types="mocha" />
+
+import {spawn, ChildProcess} from 'child_process';
+import {basename, normalize} from 'path';
+import * as ref from 'ref';
+import * as assert from 'power-assert';
+import * as nwwc from '../src/index';
+import {conf as GCF, types as GT} from 'win32-api';
+import * as Config from '../src/lib/types';
+
+const Win = nwwc.Win;
+const knl32 = Win.Kernel32.load();
+const user32 = Win.User32.load();
+const title = 'Node-Calculator';
+const waitTimeLong = '3s';
+const waitTime = '1s';
+const UC = Win.User32.constants;
+
+
+export function find_n_check_calc_win(): GT.HWND {
+    const lpszClass = Buffer.from('CalcFrame\0', 'ucs2');
+    const hWnd = user32.FindWindowExW(null, null, lpszClass, null);
+
+    if (hWnd && !ref.isNull(hWnd) && ref.address(hWnd)) {
+        assert(true);
+        change_title(hWnd, title);
+    }
+    else {
+        assert(false, 'found no calc window, GetLastError: ' + knl32.GetLastError());
+    }
+    return hWnd;
+}
+
+
+export function change_title(hWnd: GT.HWND, title: string): void {
+    if (hWnd && !ref.isNull(hWnd) && ref.address(hWnd)) {
+        const res = user32.SetWindowTextW(hWnd, Buffer.from(title + '\0', 'ucs2'));
+
+        if (!res) {
+            // See: [System Error Codes] below
+            const errcode = knl32.GetLastError();
+            const len = 255;
+            const buf = Buffer.alloc(len);
+            const p = 0x00001000 | 0x00000200;  // FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS
+            const langid = 0x0409;              // 0x0409: US, 0x0000: Neutral locale language
+            const msglen = knl32.FormatMessageW(p, null, errcode, langid, buf, len, null);
+
+            if (msglen) {
+                const errmsg = ref.reinterpretUntilZeros(buf, 2).toString('ucs2');
+                assert(false, `window found but change the title failed. errcode: ${errcode}, errmsg: "${errmsg}"`);
+            }
+        }
+        else {
+            const len = title.length;
+            const buf = Buffer.alloc(len * 3);
+            let str: string;
+
+            user32.GetWindowTextW(hWnd, buf, len + 2);
+            str = buf.toString('ucs2').slice(0, len);
+            assert(str === title.trim(), `title should be changed to ${title} in length:${title.length}, bug got ${str} in length:${str.length}`);
+        }
+    }
+    else {
+        assert(false, 'found no calc window, GetLastError: ' + knl32.GetLastError());
+    }
+
+}
+
+export function assert_execret(execRet: Config.ExecRet): void {
+    if (execRet.err) {
+        assert(false, execRet.msg);
+    }
+}
