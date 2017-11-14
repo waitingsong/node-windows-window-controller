6e326f0a6ed24fee0f593a6231f350bc0732aae4
waiting <1661926154@qq.com>
2017-11-14 16:56:09 +0800

    Add helper.ts

---
+++ src/lib/helper.ts
@@ -0,0 +1,24 @@
+import * as ref from 'ref';
+import {K} from 'win32-api';
+
+const knl32 = K.load();
+
+export function get_last_err_msg(): string {
+    // https://msdn.microsoft.com/en-us/library/windows/desktop/ms681382(v=vs.85).aspx
+    const errcode = knl32.GetLastError();
+    let errMsg = '';
+
+    if (errcode) {
+        const len = 255;
+        const buf = Buffer.alloc(len);
+        const p = 0x00001000 | 0x00000200;  // FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS
+        const langid = 0x0409;              // 0x0409: US, 0x0000: Neutral locale language
+        const msglen = knl32.FormatMessageW(p, null, errcode, langid, buf, len, null);
+
+        if (msglen) {
+            errMsg = errcode + ': ' + ref.reinterpretUntilZeros(buf, 2).toString('ucs2');
+        }
+    }
+
+    return errMsg;
+}
