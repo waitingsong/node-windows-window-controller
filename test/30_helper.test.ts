619f19b6f51122eb6b262bb33ec66b3ab4119b2c
waiting <1661926154@qq.com>
2018-06-17 17:50:19 +0800

    fix

--- test/30_helper.test.ts
+++ test/30_helper.test.ts
@@ -1,62 +1,55 @@
 /// <reference types="node" />
 /// <reference types="mocha" />
 
-import {spawn, ChildProcess} from 'child_process';
-import {basename, normalize} from 'path';
-import * as ffi from 'ffi';
-import * as ref from 'ref';
-import * as Struct from 'ref-struct';
-import * as assert from 'power-assert';
-import {fail} from 'assert';
-import * as sleep from 'mz-modules/sleep';
-import * as nwwc from '../src/index';
-import * as Config from '../src/lib/types';
-import * as H from './helper';
-import * as helper from '../src/lib/helper';
-import {DS, conf as GCF, types as GT, windef as WD} from 'win32-api';
+import { spawn, ChildProcess } from 'child_process'
+import * as sleep from 'mz-modules/sleep'
+import { basename } from 'path'
+import * as assert from 'power-assert'
+import { DModel as M } from 'win32-api'
+
+import * as nwwc from '../src/index'
+import * as helper from '../src/lib/helper'
+import * as Config from '../src/lib/types'
+
+import * as H from './helper'
 
 
-const filename = basename(__filename);
-const Win = nwwc.Win;
-const knl32 = Win.Kernel32.load();
-const user32 = Win.User32.load();
-const title = 'Node-Calculator';
-const waitTimeLong = '3s';
-const waitTime = '1s';
-const UC = Win.User32.constants;
+const filename = basename(__filename)
+const title = 'Node-Calculator'
+const waitTime = '1s'
 
 describe(filename, () => {
-    describe('Should get_last_err_msg() works', () => {
-        let child: ChildProcess;
-        let hWnd: GT.HWND;
-        let opts: Config.Opts;
+  describe('Should get_last_err_msg() works', () => {
+    let child: ChildProcess
+    let hWnd: M.HWND
+    let opts: Config.Opts
 
-        beforeEach(async () => {
-            opts = <Config.Opts> {...Config.filterWinRulesDefaults};
-            await sleep(waitTime);
-            child && child.kill();
-            child = spawn('calc.exe');
-            await sleep(waitTime);
-            hWnd = H.find_n_check_calc_win();
-        });
-        afterEach(async () => {
-            await sleep(waitTime);
-            child && child.kill();
-            await sleep(waitTime);
-        });
+    beforeEach(async () => {
+      opts = <Config.Opts> { ...Config.filterWinRulesDefaults }
+      await sleep(waitTime)
+      child && child.kill()
+      child = spawn('calc.exe')
+      await sleep(waitTime)
+      hWnd = H.find_n_check_calc_win()
+    })
+    afterEach(async () => {
+      await sleep(waitTime)
+      child && child.kill()
+      await sleep(waitTime)
+    })
 
-        it('should no error', async function() {
-            opts.matchType = 'title';
-            opts.matchValue = title;
-            const execRet = await nwwc.hide(opts);
-            const lastErrMsg = helper.get_last_err_msg();
+    it('should no error', async () => {
+      opts.matchType = 'title'
+      opts.matchValue = title
+      const execRet = await nwwc.hide(opts)
+      const lastErrMsg = helper.get_last_err_msg()
 
-            if (lastErrMsg) {
-                assert(false);
-            }
-        });
+      if (lastErrMsg) {
+        assert(false)
+      }
+    })
 
-    });
+  })
 
 
-});
+})
