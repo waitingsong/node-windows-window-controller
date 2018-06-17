e37c04543c27d256ef877699b9d16bfc7712d1b9
waiting <1661926154@qq.com>
2018-06-17 18:03:56 +0800

    format

--- src/bin/hide-window.ts
+++ src/bin/hide-window.ts
@@ -14,14 +14,14 @@ import * as nwwc from '../index'
 
 const opts = nwwc.parse_cli_opts(yargs.argv)
 
-if (! opts) {
+if (!opts) {
   console.error('argv empty. arguments: --pid|title|hwnd')
-  console.log('status value See: https://msdn.microsoft.com/en-us/library/windows/desktop/ms633548(v=vs.85).aspx')
+  console.info('status value See: https://msdn.microsoft.com/en-us/library/windows/desktop/ms633548(v=vs.85).aspx')
   process.exit(1)
 }
 else {
   nwwc.hide(opts).then((execRet: nwwc.ExecRet) => {
-      console.log('process ret:', execRet)
-      process.exit(execRet.err)
-    })
+    console.info('process ret:', execRet)
+    process.exit(execRet.err)
+  })
 }
