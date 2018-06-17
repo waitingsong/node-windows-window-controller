4c7524947035c5bc6eb16606f4a9169eb34f5225
waiting <1661926154@qq.com>
2018-06-17 17:26:23 +0800

    up

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,60 +1,60 @@
-import {types as GT, User32} from 'win32-api';
+import { DModel as M, FModel as FM, User32 } from 'win32-api'
 
-const UC = User32.constants;
-export type Tno = number;
-export type Hwnd = number;
-export type matchParam = number | string;    // pid or keyword
-export type matchType = 'pid' | 'title' | 'hwnd' | null;  // hwnd is dec/hex value, not hWnd buf
+const UC = User32.constants
+export type Tno = number
+export type Hwnd = number
+export type matchParam = number | string    // pid or keyword
+export type matchType = 'pid' | 'title' | 'hwnd' | null  // hwnd is dec/hex value, not hWnd buf
 
 export interface Task {
-    readonly tno: Tno;
-    hwndSet: Set<GT.HWND>; // result buf[]
-    pidSet: Set<GT.PID>;   // result dec[]
-    matchType: matchType;
-    matchValue: matchParam;
-    errMsg: string;
+  readonly tno: Tno
+  hwndSet: Set<M.HWND> // result buf[]
+  pidSet: Set<FM.PID>   // result dec[]
+  matchType: matchType
+  matchValue: matchParam
+  errMsg: string
 }
 
-export type ErrCode = number; // 0: no error
+export type ErrCode = number // 0: no error
 export interface ExecRet {
-    err: ErrCode;
-    msg: string;
-    pids: GT.PID[];    // processed processId
-    hwnds: Hwnd[];  // processed hWnd dec, not GT.HWND
+  err: ErrCode
+  msg: string
+  pids: FM.PID[]    // processed processId
+  hwnds: Hwnd[]  // processed hWnd dec, not GT.HWND
 }
 
 export interface FilterWinRules {
-    titleExits?: boolean | null;  // default null means 'ignore'
-    includeStyle?: number | null;
-    excludeStyle?: number | null;
-    includeExStyle?: number | null;
-    excludeExStyle?: number | null;
+  titleExits?: boolean | null  // default null means 'ignore'
+  includeStyle?: number | null
+  excludeStyle?: number | null
+  includeExStyle?: number | null
+  excludeExStyle?: number | null
 }
 export const filterWinRulesDefaults = {
-    titleExits: null,
-    includeStyle: null,
-    excludeStyle: null,
-    includeExStyle: null,
-    excludeExStyle: null,
-};
+  titleExits: null,
+  includeStyle: null,
+  excludeStyle: null,
+  includeExStyle: null,
+  excludeExStyle: null,
+}
 export const showFilterRulesDefaults = {
-    titleExits: true,
-    includeStyle: UC.WS_SYSMENU,
-    excludeStyle: UC.WS_CHILD,
-    includeExStyle: null,
-    excludeExStyle: UC.WS_EX_TOOLWINDOW,
-};
+  titleExits: true,
+  includeStyle: UC.WS_SYSMENU,
+  excludeStyle: UC.WS_CHILD,
+  includeExStyle: null,
+  excludeExStyle: UC.WS_EX_TOOLWINDOW,
+}
 
 export interface CliOpts {
-    title?: string;
-    pid?: matchParam;
-    hwnd?: matchParam;
-    status?: matchParam;
-    [prop: string]: any;
+  title?: string
+  pid?: matchParam
+  hwnd?: matchParam
+  status?: matchParam
+  [prop: string]: any
 }
 
 export interface Opts extends FilterWinRules {
-    matchType: matchType;
-    matchValue: matchParam;
-    nCmdShow?: number;
+  matchType: matchType
+  matchValue: matchParam
+  nCmdShow?: number
 }
