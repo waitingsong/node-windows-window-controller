67f28595854f42d2e6bea88d5fcc4e6db60d63b6
waiting <1661926154@qq.com>
2018-06-17 18:02:21 +0800

    format

--- src/bin/hide-window.ts
+++ src/bin/hide-window.ts
@@ -8,19 +8,20 @@
  * --hwnd={integer}
  */
 
-import * as yargs from 'yargs';
-import * as nwwc from '../index';
+import * as yargs from 'yargs'
 
-const opts = nwwc.parse_cli_opts(yargs.argv);
+import * as nwwc from '../index'
+
+const opts = nwwc.parse_cli_opts(yargs.argv)
 
 if (! opts) {
-    console.error('argv empty. arguments: --pid|title|hwnd');
-    console.log('status value See: https://msdn.microsoft.com/en-us/library/windows/desktop/ms633548(v=vs.85).aspx');
-    process.exit(1);
+  console.error('argv empty. arguments: --pid|title|hwnd')
+  console.log('status value See: https://msdn.microsoft.com/en-us/library/windows/desktop/ms633548(v=vs.85).aspx')
+  process.exit(1)
 }
 else {
-    nwwc.hide(opts).then((execRet: nwwc.ExecRet) => {
-        console.log('process ret:', execRet);
-        process.exit(execRet.err);
-    });
+  nwwc.hide(opts).then((execRet: nwwc.ExecRet) => {
+      console.log('process ret:', execRet)
+      process.exit(execRet.err)
+    })
 }
--- src/bin/kill-window.ts
+++ src/bin/kill-window.ts
@@ -8,18 +8,19 @@
  * --hwnd={integer}
  */
 
-import * as yargs from 'yargs';
-import * as nwwc from '../index';
+import * as yargs from 'yargs'
 
-const opts = nwwc.parse_cli_opts(yargs.argv);
+import * as nwwc from '../index'
+
+const opts = nwwc.parse_cli_opts(yargs.argv)
 
 if (! opts) {
-    console.error('argv empty. options: --title or --pid');
-    process.exit(1);
+  console.error('argv empty. options: --title or --pid')
+  process.exit(1)
 }
 else {
-    nwwc.kill(opts).then((execRet: nwwc.ExecRet) => {
-        console.log('process ret:', execRet);
-        process.exit(execRet.err);
-    });
+  nwwc.kill(opts).then((execRet: nwwc.ExecRet) => {
+      console.log('process ret:', execRet)
+      process.exit(execRet.err)
+    })
 }
